45a40daed710574f2274cc988024bee14c0127d9
lorenzo2506 <81492136+lorenzo2506@users.noreply.github.com>
2024-10-25 21:25:20 +0200

    Modificato menu, aggiunte traduzioni

--- Assets/Scripts/MainMenuFunction.cs
+++ Assets/Scripts/MainMenuFunction.cs
@@ -5,7 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuFunction : MonoBehaviour
 {
-    public GameObject shopPanel; // Riferimento al pannello dello shop
+    public GameObject shopPanel, settingsMenu; // Riferimento al pannello dello shop
+    
 
     void Start()
     {
@@ -13,6 +14,10 @@ public class MainMenuFunction : MonoBehaviour
         if (shopPanel != null)
         {
             shopPanel.SetActive(false);
+        }
+        if (settingsMenu != null)
+        {
+            settingsMenu.SetActive(false);
         }
     }
 
@@ -23,18 +28,30 @@ public class MainMenuFunction : MonoBehaviour
 
     public void PlayGame()
     {
+        if (settingsMenu != null)
+        {
+            settingsMenu.SetActive(false);
+        }
         SceneManager.LoadScene(1);
     }
 
     public void Restart()
     {
         // Ricarica la scena attuale
+        if (settingsMenu != null)
+        {
+            settingsMenu.SetActive(false);
+        }
         SceneManager.LoadScene(1 /*SceneManager.GetActiveScene().buildIndex*/);
     }
 
     public void GoToMenu()
     {
         // Ricarica la scena del menu principale (assicurati che sia nella posizione corretta nell'index)
+        if (settingsMenu != null)
+        {
+            settingsMenu.SetActive(false);
+        }
         SceneManager.LoadScene(0); // Sostituisci 0 con l'index della tua scena principale se necessario
     }
 
